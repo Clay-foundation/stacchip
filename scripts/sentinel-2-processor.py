694db3bd35d4190f07b80fd961f7e3c6e54a1236
Daniel Wiesmann <daniel@wiesmann.pt>
2024-03-26 17:42:35 +0000

    Uncomment upload line

--- scripts/sentinel-2-processor.py
+++ scripts/sentinel-2-processor.py
@@ -70,7 +70,7 @@ for id, row in data.iterrows():
                     new_key = (
                         f"sentinel-2-l2a/{item.id}/{Path(item.assets[key].href).name}"
                     )
-                    # s3.meta.client.copy(copy_source, V1_BUCKET, new_key)
+                    s3.meta.client.copy(copy_source, V1_BUCKET, new_key)
                     item.assets[key].href = f"s3://{V1_BUCKET}/{new_key}"
 
             # Convert Dictionary to JSON String
