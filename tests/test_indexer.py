a33c1af60bd7795f42434589b6b1471ec760a6a6
Daniel Wiesmann <daniel@wiesmann.pt>
2024-03-28 11:12:49 +0000

    Add tests for nodata and cloud cover

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -12,7 +12,7 @@ from stacchip.indexer import (ChipIndexer, LandsatIndexer, NoStatsChipIndexer,
                               Sentinel2Indexer)
 
 
-def get_ls_mock(nodata: bool=False) -> MemoryFile:
+def get_ls_mock(nodata: bool = False) -> MemoryFile:
     meta = {
         "driver": "GTiff",
         "dtype": "uint16",
@@ -23,9 +23,9 @@ def get_ls_mock(nodata: bool=False) -> MemoryFile:
         "crs": "EPSG:3031",
         "transform": Affine(30.0, 0.0, 1517085.0, 0.0, -30.0, -1811685.0),
     }
-    length = 8331 * 8271
     data = np.zeros((1, 8331, 8271))
-    data[0, :200, :200] = 1
+    if nodata:
+        data[0, :200, :200] = 1
     memfile = MemoryFile()
     with memfile.open(**meta) as dst:
         dst.write(data)
@@ -35,8 +35,10 @@ def get_ls_mock(nodata: bool=False) -> MemoryFile:
 def rasterio_open_ls_mock(href: str) -> MemoryFile:
     return get_ls_mock()
 
+
 def rasterio_open_ls_nodata_mock(href: str) -> MemoryFile:
     return get_ls_mock(True)
+
 
 def rasterio_open_sentinel_mock(href: str) -> MemoryFile:
     meta = {
@@ -49,9 +51,14 @@ def rasterio_open_sentinel_mock(href: str) -> MemoryFile:
         "crs": "EPSG:32720",
         "transform": Affine(20.0, 0.0, 499980.0, 0.0, -20.0, 6400000.0),
     }
+    data = 5 * np.ones((1, 5490, 5490), dtype="uint16")
+    # Make first chip nodata
+    data[0, :100, :100] = 0
+    # Make second chip cloudy
+    data[0, :128, 128:192] = 1
     memfile = MemoryFile()
     with memfile.open(**meta) as dst:
-        dst.write(5 * np.ones((1, 5490, 5490), dtype="uint16"))
+        dst.write(data)
     return memfile.open()
 
 
@@ -85,8 +92,9 @@ def test_sentinel_2_indexer():
     indexer = Sentinel2Indexer(item)
     assert indexer.shape == [10980, 10980]
     index = indexer.create_index()
-    assert str(index.column("chipid")[0]) == "S2A_T20HNJ_20240311T140636_L2A-0-0"
-    assert index.shape == (1764, 8)
+    assert index.shape == (1763, 8)
+    assert str(index.column("chipid")[0]) == "S2A_T20HNJ_20240311T140636_L2A-256-0"
+    assert index.column("cloud_cover_percentage")[0].as_py() == 0.5
 
 
 @mock.patch("stacchip.indexer.rasterio.open", rasterio_open_ls_mock)
@@ -113,6 +121,10 @@ def test_landsat_indexer_nodata():
     indexer = LandsatIndexer(item)
     index = indexer.create_index()
     assert index.shape == (1023, 8)
+    assert (
+        str(index.column("chipid")[0])
+        == "LC09_L2SR_086107_20240311_20240312_02_T2_SR-256-0"
+    )
 
     indexer = LandsatIndexer(item, chip_max_nodata=0.95)
     index = indexer.create_index()
