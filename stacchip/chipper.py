530494fadcd115952a13d128710f8dcf8f789cf5
Daniel Wiesmann <daniel@wiesmann.pt>
2024-04-03 11:59:32 +0100

    Convert chipper into class

--- stacchip/chipper.py
+++ stacchip/chipper.py
@@ -1,10 +1,11 @@
 import json
 import math
+from multiprocessing import Pool
 
 import boto3
-import geoarrow.pyarrow.dataset as gads
 import rasterio
-from pystac import Item
+from numpy.typing import ArrayLike
+from pystac import Asset, Item
 from rasterio.enums import Resampling
 from rasterio.windows import Window
 
@@ -13,72 +14,76 @@ from stacchip.indexer import ChipIndexer
 ASSET_BLACKLIST = ["scl", "qa_pixel"]
 
 
-def chip(index: str, row: int) -> dict:
+class Chipper:
 
-    ds = gads.dataset(index, format="parquet")
-    table = ds.to_table()
+    def __init__(
+        self,
+        bucket: str,
+        platform: str,
+        item_id: str,
+        chip_index_x: int,
+        chip_index_y: int,
+    ) -> None:
+        self.chip_index_x = chip_index_x
+        self.chip_index_y = chip_index_y
+        self.indexer = self.load_inexer(bucket, platform, item_id)
 
-    platform = table.column("platform")[row]
-    item_id = table.column("item")[row]
+    def load_inexer(self, bucket: str, platform: str, item_id: str) -> ChipIndexer:
+        s3 = boto3.resource("s3")
+        s3_bucket = s3.Bucket(name=bucket)
+        content_object = s3_bucket.Object(f"{platform}/{item_id}/stac_item.json")
+        file_content = content_object.get()["Body"].read().decode("utf-8")
+        json_content = json.loads(file_content)
 
-    bucket = "clay-v1-data"
-    s3 = boto3.resource("s3")
-    s3_bucket = s3.Bucket(name=bucket)
-    content_object = s3_bucket.Object(f"{platform}/{item_id}/stac_item.json")
-    file_content = content_object.get()["Body"].read().decode("utf-8")
-    json_content = json.loads(file_content)
-    item = Item.from_dict(json_content)
+        item = Item.from_dict(json_content)
+        return ChipIndexer(item)
 
-    indexer = ChipIndexer(item)
-    chip_index_x = table.column("chip_index_x")[row].as_py()
-    chip_index_y = table.column("chip_index_y")[row].as_py()
+    def get_pixels_for_asset(self, key: str) -> ArrayLike:
 
-    stac_item_shape = indexer.shape
-
-    data = {}
-
-    for key, asset in indexer.item.assets.items():
-        if key in ASSET_BLACKLIST:
-            continue
+        asset = self.indexer.item.assets[key]
 
         with rasterio.open(asset.href) as src:
             # Currently assume that different assets may be at different
             # resolutions, but are aligned and the gsd differs by an integer
             # multiplier.
-            if stac_item_shape[0] % src.height:
+            if self.indexer.shape[0] % src.height:
                 raise ValueError(
-                    "Asset height {src.height} is not a multiple of highest resolution height {stac_item_shape[0]}"  # noqa: E501
+                    f"Asset height {src.height} is not a multiple of highest resolution height {self.indexer.shape[0]}"  # noqa: E501
                 )
 
-            if stac_item_shape[1] % src.width:
+            if self.indexer.shape[1] % src.width:
                 raise ValueError(
-                    "Asset width {src.width} is not a multiple of highest resolution height {stac_item_shape[1]}"  # noqa: E501
+                    f"Asset width {src.width} is not a multiple of highest resolution height {self.indexer.shape[1]}"  # noqa: E501
                 )
 
-            factor = stac_item_shape[0] / src.height
+            factor = self.indexer.shape[0] / src.height
             if factor != 1:
                 print(
-                    f"Asset {key} is not at highest resolution using scaling factor of {factor}"  # noqa: E501
+                    f"Asset {asset.title} is not at highest resolution using scaling factor of {factor}"  # noqa: E501
                 )
 
             chip_window = Window(
-                math.floor(chip_index_y * indexer.chip_size / factor),
-                math.floor(chip_index_x * indexer.chip_size / factor),
-                math.ceil(indexer.chip_size / factor),
-                math.ceil(indexer.chip_size / factor),
+                math.floor(self.chip_index_y * self.indexer.chip_size / factor),
+                math.floor(self.chip_index_x * self.indexer.chip_size / factor),
+                math.ceil(self.indexer.chip_size / factor),
+                math.ceil(self.indexer.chip_size / factor),
             )
 
-            print(f"Chip window for asset {key} is {chip_window}")
-            pixels = src.read(
+            print(f"Chip window for asset {asset.title} is {chip_window}")
+            return src.read(
                 window=chip_window,
-                out_shape=(src.count, indexer.chip_size, indexer.chip_size),
+                out_shape=(src.count, self.indexer.chip_size, self.indexer.chip_size),
                 resampling=Resampling.nearest,
             )
 
-            data[key] = pixels
+    @property
+    def chip(self) -> dict:
 
-    return data
+        keys = [
+            key for key in self.indexer.item.assets.keys() if key not in ASSET_BLACKLIST
+        ]
 
+        with Pool(len(keys)) as p:
+            data = p.map(self.get_pixels_for_asset, keys)
 
-data = chip("/home/tam/Desktop/output", 23)
-print(data)
+        return dict(zip(keys, data))
