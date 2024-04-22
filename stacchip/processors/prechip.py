bf5399bba23a98381379b8f73a2dcb9025ed8b75
Daniel Wiesmann <daniel@wiesmann.pt>
2024-04-22 12:27:38 +0100

    Simplify prechip and add cli

--- stacchip/processors/prechip.py
+++ stacchip/processors/prechip.py
@@ -1,11 +1,14 @@
 import datetime
 import math
 import os
-from multiprocessing import Pool
-from pathlib import Path
+from io import BytesIO
+from typing import Union
 
-import geoarrow.pyarrow.dataset as gads
+import boto3
 import numpy as np
+import pyarrow as pa
+from multiprocessing import Pool, cpu_count
+from pyarrow import dataset as da
 import shapely
 
 from stacchip.chipper import Chipper
@@ -13,78 +16,89 @@ from stacchip.chipper import Chipper
 VERSION = "mode_v1_chipper_v1"
 
 
-class Prechipper:
+def normalize_timestamp(date):
 
-    def __init__(self, mountpath: str, indexpath: str, targetpath: str):
-        self.mountpath = mountpath
-        self.targetpath = Path(targetpath)
-        self.table = gads.dataset(indexpath, format="parquet").to_table()
+    week = date.isocalendar().week * 2 * np.pi / 52
+    hour = date.hour * 2 * np.pi / 24
 
-    def __len__(self):
-        return self.table.shape[0]
+    return (math.sin(week), math.cos(week)), (math.sin(hour), math.cos(hour))
 
-    def normalize_timestamp(self, date):
 
-        week = date.isocalendar().week * 2 * np.pi / 52
-        hour = date.hour * 2 * np.pi / 24
+def normalize_latlon(bounds):
+    bounds = shapely.from_wkt(bounds).bounds
+    lon = bounds[0] + (bounds[2] - bounds[0]) / 2
+    lat = bounds[1] + (bounds[3] - bounds[1]) / 2
 
-        return (math.sin(week), math.cos(week)), (math.sin(hour), math.cos(hour))
+    lat = lat * np.pi / 180
+    lon = lon * np.pi / 180
 
-    def normalize_latlon(self, bounds):
-        bounds = shapely.from_wkt(bounds).bounds
-        lon = bounds[0] + (bounds[2] - bounds[0]) / 2
-        lat = bounds[1] + (bounds[3] - bounds[1]) / 2
+    return (math.sin(lat), math.cos(lat)), (math.sin(lon), math.cos(lon))
 
-        lat = lat * np.pi / 180
-        lon = lon * np.pi / 180
 
-        return (math.sin(lat), math.cos(lat)), (math.sin(lon), math.cos(lon))
+def write_chip(
+    data_bucket: str,
+    chip_bucket: str,
+    row: int,
+    platform: str,
+    item_id: str,
+    date: Union[datetime.date, datetime.datetime],
+    chip_index_x: str,
+    chip_index_y: str,
+):
+    print(
+        "Writing chip", 
+        data_bucket,
+        chip_bucket,
+        row,
+        platform,
+        item_id,
+        date,
+        chip_index_x,
+        chip_index_y,
+    )
 
-    def write_chip(self, row: int):
-        chip_index_x = self.table.column("chip_index_x")[row].as_py()
-        chip_index_y = self.table.column("chip_index_y")[row].as_py()
-        platform = str(self.table.column("platform")[row])
+    chipper = Chipper(
+        bucket=data_bucket,
+        platform=platform,
+        item_id=item_id,
+        chip_index_x=chip_index_x,
+        chip_index_y=chip_index_y,
+    )
+    chip = chipper.chip
 
-        chipper = Chipper(
-            mountpath=self.mountpath,
-            platform=platform,
-            item_id=self.table.column("item")[row],
-            chip_index_x=chip_index_x,
-            chip_index_y=chip_index_y,
+    if platform == "naip":
+        pixels = chip["image"]
+        bands = ["red", "green", "blue", "nir"]
+    elif platform == "linz":
+        pixels = chip["asset"]
+        # Some imagery has an alpha band which we won't keep
+        if pixels.shape[0] != 3:
+            pixels = pixels[:3]
+        bands = ["red", "green", "blue"]
+    elif platform in ["landsat-c2l2-sr", "landsat-c2l1", "sentinel-2-l2a"]:
+        pixels = np.vstack(list(chip.values()))
+        bands = list(chip.keys())
+
+    if len(pixels) != len(bands):
+        raise ValueError(
+            f"Pixels shape {pixels.shape} is not equal to nr of bands {bands} for item {item_id}"
         )
-        chip = chipper.chip
 
-        if platform == "naip":
-            pixels = chip["image"]
-            bands = ["red", "green", "blue", "nir"]
-        elif platform == "linz":
-            pixels = chip["asset"]
-            # Some imagery has an alpha band which we won't keep
-            if pixels.shape[0] != 3:
-                pixels = pixels[:3]
-            bands = ["red", "green", "blue"]
-        elif platform in ["landsat-c2l2-sr", "landsat-c2l1", "sentinel-2-l2a"]:
-            pixels = np.vstack(list(chip.values()))
-            bands = list(chip.keys())
+    if isinstance(date, datetime.date):
+        # Assume noon for dates without timestamp
+        date = datetime.datetime(date.year, date.month, date.day, 12)
+    week_norm, hour_norm = normalize_timestamp(date)
 
-        if len(pixels) != len(bands):
-            raise ValueError(
-                f"Pixels shape {pixels.shape} is not equal to nr of bands {bands} for item {self.table.column('item')[row]}"
-            )
+    bounds = chipper.indexer.get_chip_bbox(chip_index_x, chip_index_y)
+    lon_norm, lat_norm = normalize_latlon(bounds)
 
-        date = self.table.column("date")[row].as_py()
-        if isinstance(date, datetime.date):
-            # Assume noon for dates without timestamp
-            date = datetime.datetime(date.year, date.month, date.day, 12)
-        week_norm, hour_norm = self.normalize_timestamp(date)
+    key = f"{VERSION}/{platform}/{date.year}/chip_{row}.npz"
+    # target.parent.mkdir(parents=True, exist_ok=True)
 
-        bounds = chipper.indexer.get_chip_bbox(chip_index_x, chip_index_y)
-        lon_norm, lat_norm = self.normalize_latlon(bounds)
-
-        target = self.targetpath / f"{VERSION}/{platform}/{date.year}/chip_{row}.npz"
-        target.parent.mkdir(parents=True, exist_ok=True)
+    client = boto3.client("s3")
+    with BytesIO() as bytes:
         np.savez_compressed(
-            file=target,
+            file=bytes,
             pixels=pixels,
             platform=platform,
             bands=bands,
@@ -96,6 +110,8 @@ class Prechipper:
             hour_norm=hour_norm,
             gsd=abs(chipper.indexer.transform[0]),
         )
+        bytes.seek(0)
+        client.upload_fileobj(Fileobj=bytes, Bucket=chip_bucket, Key=key)
 
 
 def process() -> None:
@@ -104,35 +120,48 @@ def process() -> None:
 
     if "AWS_BATCH_JOB_ARRAY_INDEX" not in os.environ:
         raise ValueError("AWS_BATCH_JOB_ARRAY_INDEX env var not set")
-    if "STACCHIP_MOUNTPATH" not in os.environ:
-        raise ValueError("STACCHIP_MOUNTPATH env var not set")
+    if "STACCHIP_DATA_BUCKET" not in os.environ:
+        raise ValueError("STACCHIP_DATA_BUCKET env var not set")
     if "STACCHIP_INDEXPATH" not in os.environ:
         raise ValueError("STACCHIP_INDEXPATH env var not set")
-    if "STACCHIP_TARGETPATH" not in os.environ:
+    if "STACCHIP_CHIP_BUCKET" not in os.environ:
         raise ValueError("STACCHIP_TARGETPATH env var not set")
 
     index = int(os.environ["AWS_BATCH_JOB_ARRAY_INDEX"])
+    data_bucket = os.environ["STACCHIP_DATA_BUCKET"]
+    indexpath = os.environ["STACCHIP_INDEXPATH"]
+    chip_bucket = os.environ["STACCHIP_CHIP_BUCKET"]
+    platform = os.environ.get("STACCHIP_PLATFORM")
 
-    prech = Prechipper(
-        mountpath=os.environ["STACCHIP_MOUNTPATH"],
-        indexpath=os.environ["STACCHIP_INDEXPATH"],
-        targetpath=os.environ["STACCHIP_TARGETPATH"],
+    # Open table
+    table = da.dataset(indexpath, format="parquet").to_table(
+        columns=["chipid", "platform", "item", "date", "chip_index_x", "chip_index_y"]
     )
+    if platform:
+        table = table.filter(pa.compute.field("platform") == platform)
 
-    # for i in range(len(prech)):
-    #     prech.write_chip(i)
-
-    range_upper_limit = min(len(prech), (index + 1) * CHIPS_PER_BATCH)
-
-    with Pool(10) as pl:
-        pl.map(
-            prech.write_chip,
-            range(index * CHIPS_PER_BATCH, range_upper_limit),
+    # Extract chips data for this job
+    range_upper_limit = min(table.shape[0], (index + 1) * CHIPS_PER_BATCH)
+    all_chips = []
+    for row in range(index * CHIPS_PER_BATCH, range_upper_limit):
+        all_chips.append(
+            (
+                data_bucket,
+                chip_bucket,
+                row,
+                table.column("platform")[row].as_py(),
+                table.column("item")[row].as_py(),
+                table.column("date")[row].as_py(),
+                table.column("chip_index_x")[row].as_py(),
+                table.column("chip_index_y")[row].as_py(),
+            )
         )
 
+    # for i in range(5):
+    #     write_chip(*all_chips[i])
 
-os.environ["STACCHIP_TARGETPATH"] = "/home/tam/Desktop/clay-v1-data-small/prechip-tiles"
-os.environ["STACCHIP_INDEXPATH"] = "/home/tam/Desktop/clay-v1-data-small/index-combined"
-os.environ["STACCHIP_MOUNTPATH"] = "/home/tam/Desktop/clay-v1-data-small"
-os.environ["AWS_BATCH_JOB_ARRAY_INDEX"] = "0"
-process()
+    with Pool(cpu_count() * 2) as pl:
+        pl.starmap(
+            write_chip,
+            all_chips[:50],
+        )
