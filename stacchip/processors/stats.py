79999d3ec13f41469fad10eb0404c5a66baf9826
Daniel Wiesmann <daniel@wiesmann.pt>
2024-04-30 13:03:06 +0100

    Add nodata mask to stats calculation

--- stacchip/processors/stats.py
+++ stacchip/processors/stats.py
@@ -4,13 +4,18 @@ import os
 import boto3
 import numpy as np
 
-from stacchip.processors.prechip import (CUBESIZE, LINZ_BANDS, LS_BANDS,
-                                         NAIP_BANDS, S1_BANDS, S2_BANDS)
+from stacchip.processors.prechip import (
+    LINZ_BANDS,
+    LS_BANDS,
+    NAIP_BANDS,
+    S1_BANDS,
+    S2_BANDS,
+)
 
 MAX_CUBES = 1000
 
 
-def get_stats_keys(key):
+def get_stats_keys(key, nodata):
     s3_session = boto3.resource("s3")
     obj = s3_session.Object("clay-v1-data-cubes", key)
     body = obj.get()["Body"].read()
@@ -20,9 +25,11 @@ def get_stats_keys(key):
 
     data = data.astype("float64").swapaxes(0, 1)
 
-    pixel_count = CUBESIZE * 256 * 256
-    pixel_sum = np.sum(data, axis=(1, 2, 3))
-    pixel_sqr = np.sum(np.power(data, 2), axis=(1, 2, 3))
+    data = np.ma.array(data, mask=data == nodata)
+
+    pixel_count = np.ma.count(data)
+    pixel_sum = np.ma.sum(data, axis=(1, 2, 3))
+    pixel_sqr = np.ma.sum(np.ma.power(data, 2), axis=(1, 2, 3))
 
     return pixel_count, pixel_sum, pixel_sqr
 
@@ -34,14 +41,19 @@ def process():
     platform = os.environ.get("STACCHIP_PLATFORM")
     if platform == "naip":
         bands = NAIP_BANDS
+        nodata = 0
     elif platform == "linz":
         bands = LINZ_BANDS
+        nodata = 0
     elif platform == "sentinel-2-l2a":
         bands = S2_BANDS
+        nodata = 0
     elif platform in ["landsat-c2l2-sr", "landsat-c2l1"]:
         bands = LS_BANDS
+        nodata = 0
     elif platform == "sentinel-1-rtc":
         bands = S1_BANDS
+        nodata = -32768
     else:
         raise ValueError(f"Platform {platform} not found")
 
@@ -63,7 +75,7 @@ def process():
             counter += 1
             print(counter, key)
 
-            px0, px1, px2 = get_stats_keys(key)
+            px0, px1, px2 = get_stats_keys(key, nodata)
 
             pixel_count = np.add(pixel_count, px0)
             pixel_sum = np.add(pixel_sum, px1)
