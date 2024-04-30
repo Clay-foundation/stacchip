d02aab6e37b6b13190696f4975ee02255852f129
Daniel Wiesmann <daniel@wiesmann.pt>
2024-04-30 14:56:04 +0100

    Fix count use multiprocessing

--- stacchip/processors/stats.py
+++ stacchip/processors/stats.py
@@ -1,21 +1,21 @@
 import io
 import os
+from multiprocessing import Pool
 
 import boto3
 import numpy as np
 
-from stacchip.processors.prechip import (
-    LINZ_BANDS,
-    LS_BANDS,
-    NAIP_BANDS,
-    S1_BANDS,
-    S2_BANDS,
-)
-
-MAX_CUBES = 1000
+from stacchip.processors.prechip import (LINZ_BANDS, LS_BANDS, NAIP_BANDS,
+                                         S1_BANDS, S2_BANDS)
 
 
-def get_stats_keys(key, nodata):
+def get_stats_keys(key):
+    print(f"Processing {key}")
+    if "sentinel-1-rtc" in key:
+        nodata = -32768
+    else:
+        nodata = 0
+
     s3_session = boto3.resource("s3")
     obj = s3_session.Object("clay-v1-data-cubes", key)
     body = obj.get()["Body"].read()
@@ -27,7 +27,7 @@ def get_stats_keys(key, nodata):
 
     data = np.ma.array(data, mask=data == nodata)
 
-    pixel_count = np.ma.count(data)
+    pixel_count = np.ma.count(data, axis=(1, 2, 3))
     pixel_sum = np.ma.sum(data, axis=(1, 2, 3))
     pixel_sqr = np.ma.sum(np.ma.power(data, 2), axis=(1, 2, 3))
 
@@ -37,23 +37,20 @@ def get_stats_keys(key, nodata):
 def process():
     if "STACCHIP_PLATFORM" not in os.environ:
         raise ValueError("STACCHIP_PLATFORM env var not set")
+    pool_size = os.environ.get("STACCHIP_POOL_SIZE", 4)
+    max_cubes = os.environ.get("STACCHIP_MAX_CUBES", 4)
 
     platform = os.environ.get("STACCHIP_PLATFORM")
     if platform == "naip":
         bands = NAIP_BANDS
-        nodata = 0
     elif platform == "linz":
         bands = LINZ_BANDS
-        nodata = 0
     elif platform == "sentinel-2-l2a":
         bands = S2_BANDS
-        nodata = 0
     elif platform in ["landsat-c2l2-sr", "landsat-c2l1"]:
         bands = LS_BANDS
-        nodata = 0
     elif platform == "sentinel-1-rtc":
         bands = S1_BANDS
-        nodata = -32768
     else:
         raise ValueError(f"Platform {platform} not found")
 
@@ -69,25 +66,33 @@ def process():
     pixel_sqr = np.zeros(band_count)
 
     counter = 0
+    all_keys = []
     for page in page_iterator:
         keys = [dat["Key"] for dat in page["Contents"]]
         for key in keys:
             counter += 1
-            print(counter, key)
+            all_keys.append(key)
+            if counter == max_cubes:
+                break
+        if counter == max_cubes:
+            break
 
-            px0, px1, px2 = get_stats_keys(key, nodata)
+    with Pool(pool_size) as pl:
+        result = pl.map(get_stats_keys, all_keys)
 
-            pixel_count = np.add(pixel_count, px0)
-            pixel_sum = np.add(pixel_sum, px1)
-            pixel_sqr = np.add(pixel_sqr, px2)
+    for dat in result:
+        pixel_count = np.add(pixel_count, dat[0])
+        pixel_sum = np.add(pixel_sum, dat[1])
+        pixel_sqr = np.add(pixel_sqr, dat[2])
 
-            # https://stackoverflow.com/questions/1174984/how-to-efficiently-calculate-a-running-standard-deviation
-            mean = pixel_sum / pixel_count
-            stdev = np.sqrt((pixel_sqr / pixel_count) - (mean * mean))
+    # https://stackoverflow.com/questions/1174984/how-to-efficiently-calculate-a-running-standard-deviation
+    mean = pixel_sum / pixel_count
+    stdev = np.sqrt((pixel_sqr / pixel_count) - (mean * mean))
 
-            print(f"Progressive stats Mean {dict(zip(bands, mean))}")
-            print(f"Progressive stats Stdev {dict(zip(bands, stdev))}")
+    print("-- Mean by band")
+    for band, val in zip(bands, mean):
+        print(f"{band}: {val}")
 
-            if counter == MAX_CUBES:
-                print(f"Finished after {MAX_CUBES} cubes")
-                return
+    print("-- Std by band")
+    for band, val in zip(bands, stdev):
+        print(f"{band}: {val}")
