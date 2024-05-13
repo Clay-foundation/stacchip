0702d363b029f397d3d11a5e0561969d5849d66c
Daniel Wiesmann <daniel@wiesmann.pt>
2024-05-13 16:58:08 +0100

    Linted code

--- stacchip/__init__.py
+++ stacchip/__init__.py
@@ -1,10 +1,1 @@
 __version__ = "0.1.30"
-
-from .chipper import Chipper  # noqa
-from .indexer import (
-    ChipIndexer,
-    LandsatIndexer,
-    NoDataMaskChipIndexer,
-    NoStatsChipIndexer,
-    Sentinel2Indexer,
-)
